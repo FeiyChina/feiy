e78afcfe930aea3a5919f3d13ea3ba6017d8f996
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-04-18 15:24:47 +0800

    l

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   delete 'organizations/:id', to: "organizations#destroy"
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root to: 'pages#home'
 
 end
