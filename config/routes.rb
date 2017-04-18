c48fb1ba31fdaaa8af296a73d68f14e2e649d8e1
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-04-18 15:36:50 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'organizations/:id', to: "organizations#show"
+  # get 'organizations/:id', to: "organizations#show"
 
-  get 'organizations/new', to: "organizations#new"
-  post 'organizations', to: "organizations#create"
+  # get 'organizations/new', to: "organizations#new"
+  # post 'organizations', to: "organizations#create"
 
-  get 'organizations/:id/edit', to: "organizations#edit"
-  patch 'organizations/:id', to: "organizations#update"
+  # get 'organizations/:id/edit', to: "organizations#edit"
+  # patch 'organizations/:id', to: "organizations#update"
 
-  delete 'organizations/:id', to: "organizations#destroy"
-
+  # delete 'organizations/:id', to: "organizations#destroy"
+  resources :organizations
   devise_for :users
 
   root to: 'pages#home'
