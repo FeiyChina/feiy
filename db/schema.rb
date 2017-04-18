6ddf9f6332214ba40fb619c89c42f9262fb5733d
Patricia Wiesker <patricia@wiesker.de>
2017-04-18 11:18:25 +0800

    organization table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418025204) do
+ActiveRecord::Schema.define(version: 20170418025251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "name"
+    t.integer  "problem"
+    t.integer  "description"
+    t.integer  "website"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_organizations_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +43,5 @@ ActiveRecord::Schema.define(version: 20170418025204) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "organizations", "users"
 end
