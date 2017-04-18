80b70c5adf60b36a8c2e25d887a9adc8464b7b53
Sebastian Nugnez <sebastian.nugnez@gmail.com>
2017-04-18 12:10:39 +0800

    added some js for cloudinary (config 2 karr)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-fileupload/basic
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 //= require_tree .
