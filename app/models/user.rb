6afed6c35945674b40ee6ab873ec1d8617163d7e
Patricia Wiesker <patricia@wiesker.de>
2017-04-18 16:35:17 +0800

    Changed type of each column in Organizations table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :organizations
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
