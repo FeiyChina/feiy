5dbbde2bebaada7e4fb06ac916aa5b465574c0fe
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-20 15:45:06 +0800

    organization has_many :categories

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,7 @@
 class Organization < ApplicationRecord
   has_attachments :photos, maximum: 3
-
   acts_as_commentable
   acts_as_votable
-
   belongs_to :user
+  has_many :categories, as: :categorizable
 end
