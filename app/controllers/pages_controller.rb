513c4e8ede8a17bb2f910cc4c39309348b65b678
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-21 10:00:23 +0800

    non logged can click event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  before_action :authenticate_user!, only: :dashboard
 
   def home
     @current_user = current_user
