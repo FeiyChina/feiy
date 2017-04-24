835aa7c60f0956b0efeb9c62e3a444fd59841141
Michael Chee <michaelchee@Michaels-MacBook-Pro.local>
2017-04-24 13:19:02 +0800

    generated jobs model and schema

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,17 @@
+class JobsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ db/migrate/20170424040413_create_jobs.rb
@@ -0,0 +1,16 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string  :name
+      t.string  :address
+      t.string  :website
+      t.string  :email
+      t.string  :type, inclusion: { in: %w(volunteer internship part-time full-time)}
+      t.string  :task
+      t.string  :requirement
+      t.boolean :active, default: true
+
+      t.timestamps
+    end
+  end
+end
