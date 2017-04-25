06b71f59dc79973d74902a9b43ed25ebb26183a7
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-25 19:44:14 +0800

    fixed bug on the views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,19 +5,14 @@ class PagesController < ApplicationController
     @current_user = current_user
     @organizations = Organization.all
     @categories = Category.all
-
-
     @job = Job.all
     @jobs = @job.where(active: true)
-    
   end
 
   def dashboard
     @current_user = current_user
     @organizations = @current_user.organizations
     @categories = Category.all
-
-
     @jobs = Job.all
   end
 
