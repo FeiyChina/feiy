8dd165f6e3f3583a9e631ea6ea28cb605f8d78e6
Stephane Delgado <stephanedelgado@gmail.com>
2017-04-27 16:01:43 +0800

    added validations and regex

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
