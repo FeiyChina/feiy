285641e07327f1219c7eee6f76fd5a899d881e25
Stephane Delgado <stephanedelgado@gmail.com>
2017-05-01 22:50:53 +0800

    fixed categories for organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -8,12 +8,14 @@ class OrganizationsController < ApplicationController
 
   def new
     @organization = Organization.new
-    @categories = Category.all
+    @categories = ENV["categories"].split(",")
+    @current_category = ENV["categories"].first
   end
 
   def create
     @organization = Organization.new(organization_params)
     @organization.user_id = current_user.id
+    @organization.categories << Category.create(name: params[:organization][:categories])
     if @organization.save
       redirect_to dashboard_path
     else
@@ -23,11 +25,23 @@ class OrganizationsController < ApplicationController
 
   def edit
     @organization = Organization.find(params[:id])
+    @categories = ENV["categories"].split(",")
+    if @organization.categories.any?
+      @current_category = @organization.categories.last.name
+    else
+      @current_category = ENV["categories"].first
+    end
   end
 
   def update
     @organization = Organization.find(params[:id])
     @organization.update(organization_params)
+    if @organization.categories.any?
+      @organization.categories.last.update(name: params[:organization][:categories])
+    else
+      @organization.categories << Category.create(name: params[:organization][:categories])
+      @organization.save
+    end
     redirect_to organization_path(@organization)
   end
 
@@ -73,6 +87,6 @@ class OrganizationsController < ApplicationController
   end
 
   def organization_params
-    organization_params = params.require(:organization).permit(:name, :problem, :description, :website, :address, :photo, :logo, :category_ids, :user_is_a_representative)
+    organization_params = params.require(:organization).permit(:name, :problem, :description, :website, :address, :photo, :logo, :user_is_a_representative, :category_ids)
   end
 end
