9e911d263ce29232e0830b0a68c4aa863d4e3bb9
Michael Chee <michaelchee@Michaels-MacBook-Pro.local>
2017-05-02 10:07:14 +0800

    added new, create, edit, update toe events ctrl

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,5 +6,40 @@ class EventsController < ApplicationController
   #   redirect_to event_show_path
   # end
 
+  def new
+    @organization = Organization(params[:organization_id])
+    @event = Event.new
+  end
+
+  def create
+    @event = event.new(event_params)
+    @event.organization = Organization.find(params[:organization_id])
+    @event.save
+      redirect_to dashboard_path
+  end
+
+  def edit
+    @organization = Organization.find(params[:organization_id])
+    @event = Event.find(params[:id])
+  end
+
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :content, :date, :venue, :job_type, :task, :requirement, :active )
+  end
+
 
 end
+
+
+
