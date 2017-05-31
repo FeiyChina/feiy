c80e3e8c16aecc7c94a40e0826352df06302681f
dounan <user@Users-Mac.local>
2017-05-31 22:42:14 +0800

    mixpanel device fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,77 +1,38 @@
-# class RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
 
-#   def create
-#     super
+  def create
+    super
 
-#     if resource.save
-#       @user = current_user
-
-#       MIXPANEL.track(@user.id, 'Created User', {
-#           first_name: @user.name,
-#           email: @user.email
-#       })
-#       MIXPANEL.people.set(@user.id, {
-#           first_name: @user.name,
-#           email: @user.email
-#       })
-
-#       # Mixpanel Create User Profile
-#       # mixpanel.track(@user.id, 'Created User', {
-#       #   '$first_name' => @user.first_name,
-#       #   '$gametag' => @user.game_tag,
-#       #   '$email' => @user.email,
-#       # })
-#       # mixpanel.people.set(@user.id, {
-#       #   '$first_name' => @user.first_name,
-#       #   '$gametag' => @user.game_tag,
-#       #   '$email' => @user.email,
-#       # })
-#     end
-#   end
-# end
+    if resource.persisted?
+      @user = current_user
+      track_registration(resource)
+    end
+  end
 
 
+  private
+
+  def track_registration(resource)
+
+    user = resource
+    MIXPANEL.people.set(
+      user.id,
+      {
+          "$first_name" => user.name,
+          "$email" => user.email,
+          "ip" => request.env['REMOTE_ADDR']
+      }
+    )
 
 
-  # def track_registration(resource)
+  end
 
-  #       user = this.current_user
-  #   MIXPANEL.people.set(user.id, {
-  #       $first_name: user.name,
-  #       $email: user.email,
-  #       ip: current_sign_in_ip
-  #     });
-
-
-  # end
-
-    # if @organization.save
-    #   MIXPANEL.track(@organization.user_id, 'Created', {
-    #     content: "Organization",
-    #     name: @organization.name
-    #   })
-
-    #   redirect_to dashboard_path
-    # else
-    #   render :new
-    # end
+end
 
 
 
 
 
-  # def create
-  #   super
-  #   track_sign_in(this.resource) if signed_in?
-  #   puts this.resource.to_s
-  # end
 
-  # private
-  # def track_sign_in(resource)
-  #   user = this.current_user
-  #   MIXPANEL.people.set(user.id, {
-  #       first_name: user.name,
-  #       email: user.email,
-  #       ip: current_sign_in_ip
-  #     });
-  # end
+
+
