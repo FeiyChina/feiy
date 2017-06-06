fa79a83c0444367e987528d7ff0212a9e3b0f8e0
dounan <user@Users-Mac.local>
2017-06-06 18:43:18 +0800

    drop likes table only if exists

--- db/migrate/20170517064830_drop_likes_table.rb
+++ db/migrate/20170517064830_drop_likes_table.rb
@@ -1,6 +1,8 @@
 class DropLikesTable < ActiveRecord::Migration[5.0]
   def up
-    drop_table :likes
+    if ActiveRecord::Base.connection.data_source_exists? 'likes'
+      drop_table :likes
+    end
   end
 
   def down
