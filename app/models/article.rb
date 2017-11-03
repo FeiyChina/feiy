ffd643e56a1c555d3d0fe62271205160945203c6
Maksim <m.glinka@rambler-co.ru>
2017-11-03 13:13:36 +0300

    Adds publication logic, adds main article logic, adds pagination 6 per page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,34 @@
 class Article < ApplicationRecord
-  scope :accepted, -> { where(accepted?: true) }
+  scope :published, -> { where(is_published: true, is_main_article: false).order(published_at: :desc) }
+  scope :main_article, -> { where(is_main_article: true) }
+  before_save :check_for_publication_status
+  before_save :set_main_article
   has_attachment :photo
   acts_as_commentable
   acts_as_votable
   belongs_to :user
+
+  def self.decide_page(page)
+    if !page || page == '1'
+      Article.published.page(1).per(6)
+    elsif page == '2'
+      Article.published.page(2).per(6)
+    else
+      Article.published.page(page).per(6)
+    end
+  end
+
+  private
+
+  def check_for_publication_status
+    if (is_published_was == false) && (is_published == true) && published_at_was.nil?
+      self.published_at = Time.zone.now
+    end
+  end
+
+  def set_main_article
+    if (is_main_article_was == false) && (is_main_article == true)
+      Article.where.not(id: id).update_all(is_main_article: false)
+    end
+  end
 end
