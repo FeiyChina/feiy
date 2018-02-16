d40681d83976b51934c11486c2dae9b64d95a54e
Maksim <xnagpa@outlook.com>
2018-02-16 07:43:45 +0300

    [disappearing-logo-issue] Adds validations and condition

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,8 @@
 class Organization < ApplicationRecord
   acts_as_taggable
   scope :accepted, -> { where(accepted?: true) }
-  has_attachment :logo
-  has_attachment :photo
+  has_attachment :logo, accept: [:jpg, :png, :gif]
+  has_attachment :photo, accept: [:jpg, :png, :gif]
   acts_as_commentable
   acts_as_votable
   belongs_to :user
