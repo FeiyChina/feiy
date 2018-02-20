aade52601ee59551b011889dc7da3610eff40522
Maksim <xnagpa@outlook.com>
2018-02-20 23:43:41 +0300

    Refines style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      redirect_to new_user_registration_path unless current_page?(new_user_registration_path) || current_page?(new_user_session)
+      redirect_to new_user_registration_path unless current_page?(new_user_registration_path) || current_page?(new_user_session_path)
     end
   end
 
