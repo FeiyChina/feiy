a99483ddbfa6837871531383960a26c58f376745
Maksim <xnagpa@outlook.com>
2018-03-15 21:23:21 +0300

    [hotfix] Temporarily switch off GA

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,3 +1,3 @@
 $(document).on('turbolinks:load', function () {
-gtag('config', 'UA-106070409-1', {'page_path': window.location.pathname});
+//gtag('config', 'UA-106070409-1', {'page_path': window.location.pathname});
 });
