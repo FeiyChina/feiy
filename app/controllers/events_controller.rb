5fce7d37894d63a4bc9c0afe25bc3b9234a5876e
Maksim <xnagpa@outlook.com>
2018-03-25 16:29:29 +0300

    Improves events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,8 +50,7 @@ class EventsController < ApplicationController
   def show
     @organization = Organization.find(params[:organization_id])
     @event = Event.find(params[:id])
-    @baidumap_url = 'kekus'
-    #@baidumap_url = 'http://api.map.baidu.com/staticimage/v2?ak=' + ENV['BAIDU_KEY'] + "&amp;" + 'mcode=666666&center=' + @event.longitude.to_s + ',' + @event.latitude.to_s + '&width=580&height=250&zoom=16'
+    @baidumap_url = 'http://api.map.baidu.com/staticimage/v2?ak=' + ENV['BAIDU_KEY'] + "&amp;" + 'mcode=666666&center=' + @event.longitude.to_s + ',' + @event.latitude.to_s + '&width=580&height=250&zoom=16'
   end
 
   def destroy
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.default_url_options = { host: 'feiy.herokuapp.com' }
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'otakusan113@gmail.com',
-    :password             => 'SAURHOHNSMEAGORHL1984',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
