2db7a0252bec2b27b6da91856870ef0b69944499
Maksim <m.glinka@rambler-co.ru>
2019-08-06 18:34:38 +0300

    [improve-cloudinary] Updates active admin so cloudinary could work again

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,9 @@
+//= require active_admin/base
+//= require jquery-fileupload/vendor/jquery.ui.widget
+//= require jquery-fileupload/jquery.iframe-transport
+//= require jquery-fileupload/jquery.fileupload
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
+//= require attachments
+//= require active_admin/base
+//= require tinymce
